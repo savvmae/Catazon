b67f2bc9a76a2144022e2dd8231f09b37e788a9f
savannah cosby <savannah.cosby@union.co>
2018-10-03 16:57:42 -0400

    Optimizations and playing with structures. Clean up

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import { Subscribe } from 'unstated';
 
-import CartContainer from './CartContainer';
+import CartContainer from '../Containers/CartContainer';
 
 export default class Cart extends React.Component {
   render() {
     return (
-      <ul>
+      <div className="Cart">
         <Subscribe to={[CartContainer]}>
           {cartContainer =>
             cartContainer.state.cartItems.map(item => (
-              <li key={item.id}>
-                  <img src={item.img} alt="cat pic" />
-                  <p>{item.description} </p>
-                  <p>{item.price} </p>
-                    <button onClick={() => cartContainer.removeItem(item.id)}>
-                    Remove Item From Cart
-                    </button>
-              </li>
+              <div className="Cart-item" key={item.id}>
+                <img src={item.img} alt="cat pic" />
+                <p className="Cart-item-description">{item.description} </p>
+                <p className="Cart-item-price">{item.price} </p>
+                <button onClick={() => cartContainer.removeItem(item.id)}>
+                Remove Item From Cart
+                </button>
+              </div>
             ))
           }
         </Subscribe>
-      </ul>
+      </div>
     );
   }
 }
--- src/Components/Items.js
+++ src/Components/Items.js
@@ -1,32 +1,29 @@
 import React from 'react';
 import { Subscribe } from 'unstated';
 
-import CartContainer from './CartContainer';
-
-import Cats from '../Config/Items';
+import CartContainer from '../Containers/CartContainer';
 
 export default class Items extends React.Component {
   render() {
     return (
-      <ul>
-        {Cats.map(cat => (
-            <li key={cat.id}>
-                <h5>{cat.name}</h5>
-                <p>{cat.description}</p>
-                <img src={cat.img} alt="cat pic" />
-                <p>${cat.price}</p>
-
-                <Subscribe to={[CartContainer]}>
-                    {cartContainer => (
+      <div>
+        <Subscribe to={[CartContainer]}>
+            {cartContainer => 
+                cartContainer.state.items.map(cat => (
+                    <div key={cat.id}>
+                        <h5>{cat.name}</h5>
+                        <p>{cat.description}</p>
+                        <img src={cat.img} alt="cat pic" />
+                        <p>${cat.price}</p>
+                            
                         <button onClick={() => cartContainer.addItem(cat)}>
                             Add Cat to Cart
                         </button>
-                    )}
-                </Subscribe>
-            </li>
-        ))
-        }
-      </ul>
+                    </div>
+                ))
+            }
+        </Subscribe>
+      </div>
     );
   }
 }
